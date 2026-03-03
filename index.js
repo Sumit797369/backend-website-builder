faf1417aab9aa58bff08e2e6d9bfb52e0ba42226
Sumit Kumar <tbeast935@gmail.com>
2026-03-03 23:22:07 +0530

    auth setup

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express"
 import dotenv from "dotenv"
-import connectDB from "./config/db.js";
 dotenv.config();
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.routes.js";
 const app = express();
 const port = process.env.PORT || 5000
-
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin:"http:localhost:5173",
+    Credentials:true
+}))
+app.use("/api/auth",authRouter)
 app.listen(port,()=>{
     console.log("server started");
     connectDB()
