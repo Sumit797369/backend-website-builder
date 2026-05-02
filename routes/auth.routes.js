7ef44abb8e90a5e38b09249512ad5aee9809876a
Sumit Kumar <tbeast935@gmail.com>
2026-05-02 12:13:30 +0530

    auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,20 @@
 import express from "express"
-import { googleAuth, logOut } from "../controllers/auth.js"
+import { googleAuth, logOut,register,login } from "../controllers/auth.js"
+import { isAuth } from "../middleware/authmiddleware.js"
 
-const authRouter=express.Router()
 
-authRouter.post("/google",googleAuth)
-authRouter.get("/google",logOut)
+const authRouter = express.Router()
 
+
+authRouter.post("/google", googleAuth)
+
+authRouter.post("/register", register)
+authRouter.post("/login", login)
+authRouter.post("/logout", logOut)
+
+
+authRouter.get("/me", isAuth, (req, res) => {
+  res.json({ message: "User is authenticated", userId: req.userId })
+})
 
 export default authRouter
