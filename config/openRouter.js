ffaa414f318a4dcaec131257119e62e1e9dd2f25
Sumit Kumar <tbeast935@gmail.com>
2026-05-05 18:33:10 +0530

    api

---
+++ config/openRouter.js
@@ -0,0 +1,23 @@
+const openRouterUrl = "https://openrouter.ai/api/v1/chat/completions";
+const openRouetrApiKey = process.env.OPENROUTER_API_KEY;
+const model = "deepseek/deepseek-chat";
+
+const generateResponse = async (prompt) => {
+  const res = await fetch(openRouterUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${openRouetrApiKey}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: model,
+      messages: [
+        { role: "system", content: "Your must return only valid raw JSON." },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    }),
+  });
+};
